6e868a99f88a8a18fba1b004db2ec56ec1c650c5
MHaris-Ferrum <muhammad.haris@ferrum.network>
2024-06-24 16:58:35 +0500

    workflow file updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From Youtube
+          Learn React From AnyWhere
         </a>
       </header>
     </div>
